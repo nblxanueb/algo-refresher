e93c4cd36d7c1be88f32fdfa29f39cf8189459a7
andre <a.e.pykhantsev@gmail.com>
2018-09-21 02:23:31 -0400

    palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -13,3 +13,9 @@ function palindrome(str) {
 }
 
 module.exports = palindrome;
+
+// function palindrome(str) {
+//   return str.split("").every((char, i) => {
+//     return char == str[str.length - i - 1];
+//   });
+// }
